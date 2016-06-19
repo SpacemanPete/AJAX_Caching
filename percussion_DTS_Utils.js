4701080b322098400e9761ae6a1920d01a8c17b9
Piotr Butkiewicz <piotrab@gmail.com>
2016-06-19 01:08:48 -0400

    Added stub file for Perc DTS utilis script

---
+++ percussion_DTS_Utils.js
@@ -0,0 +1,13 @@
+// ************************************************************************
+//
+//  Percussion DTS Utilities 
+//  Written by Piotr Butkiewicz of Percussion Software
+//
+//  Function:
+//    A collection of JavaScript utilities for working with   
+//    Percussion DTS Metadata Service
+//
+//  Dependencies:
+//    None.
+//
+// ************************************************************************
