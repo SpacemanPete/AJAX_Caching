b020223960c2d26e034af71b1d4f37c4b4809576
Piotr Butkiewicz <piotrab@gmail.com>
2016-06-20 20:12:25 -0400

    First working draft of PercUtils for retrieving and cachign DTS data. Using Revealing Module Pattern.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Perc_JS_Utils
+#Perc_JS_Utils
 Javascript utilities API.
+
+##PercUtils.cachedAjaxPromise 
+    Function to retieve data from a remote server via AJAX request and cache response for specified period of time.
+    Works with JSON and XML data server responses. 
+
+    PercUtils.cachedAjaxPromise(URL, ajaxOptions, TTL, storageType).then(successCallback, errorCallback);
+    
+
+###Options
+
+####URL - Required
+    Enter URL of destination for data retrieval.
+    Type: String
+
+####ajaxOptions - Optional
+    Enter optional AJAX options.
+    Type: Object {}
+    Example: 
+        {
+            type: "POST",
+            data: queryCriteria,
+            contentType: 'application/json',
+        }
+
+####TTL - Required
+    Enter a time-to-live for the cached data in minutes.
+    Type: Number
+
+####storageType
+    Enter the type of storage to save data to.
+    Type: String
+    Example: 
+        "localStorage"
+        "sessionStorage"
+
+###Usage
+
+    PercUtils.cachedAjaxPromise(
+        jQuery.getDeliveryServiceBase() + '/perc-metadata-services/metadata/get',
+        {
+            type: "POST",
+            data: queryCriteria,
+            contentType: 'application/json',
+        },
+        1, 
+        "sessionStorage"
+    ).then(successCallback, errorCallback);
+
--- percussion_DTS_Utils.js
+++ percussion_DTS_Utils.js
@@ -12,15 +12,103 @@
 //
 // ************************************************************************
 
-$.createCache = function( requestFunction ) {
-    return function( key, callback ) {
-        if ( !lscache.get(key) ) {
-            lscache.set(key, function(){
-                return $.Deferred(function( defer ) {
-                    requestFunction( defer, key );
-                }).promise();
-            });
+var PercUtils = (function ($) {
+    var oKeyDeferredMap = {};
+
+    function readFromStorage(key) {
+        var sValue = percStorage.load(key, "sessionStorage");
+        return sValue;
+    }
+
+    function writeToStorage(key, DtsData, xhr, TTL) {
+        var dataType = xhr.getResponseHeader("content-type");
+        percStorage.save(key, DtsData, TTL, "sessionStorage", dataType);
+        console.info('Storing AJAX Response data to local Cache');
+    }
+    
+    var cachedAjaxPromise = function (url, ajaxOptions, TTL, storageType) {
+        var oDeferred = oKeyDeferredMap[url];
+        var cacheAvailable = storageAvailable(storageType);
+        var sValue;
+
+        if (!oDeferred) {
+            oDeferred = new jQuery.Deferred();
+            oKeyDeferredMap[url] = oDeferred;
+            if (cacheAvailable){
+                sValue = readFromStorage(url);
+            }
+
+            if (sValue) {
+                console.info('Cached Data found for AJAX query: ');
+                console.info(url);
+                oDeferred.resolve(sValue);
+            } else {
+                if (!ajaxOptions) {
+                    ajaxOptions = {};
+                }
+
+                $.extend(ajaxOptions, {
+                    error: function (xhr, textStatus, errorThrown) {
+                        console.error('customer info request failed: ' + errorThrown);
+                        oDeferred.reject();
+                    },
+                    success: function (DtsData, status, xhr) {
+                        console.info('AJAX query successful');
+                        if(cacheAvailable){
+                            writeToStorage(url, DtsData, xhr, TTL);
+                        }
+                        oDeferred.resolve(DtsData);
+                    }
+                });
+
+                $.ajax(url, ajaxOptions);
+            }
         }
-        return cache[ key ].done( callback );
+        return oDeferred.promise();
     };
-}
+
+    var percStorage = {
+        save : function(key, data, TTL, storageType, dataType){
+            if (!(storageAvailable(storageType))){return false;}
+            var TTL = TTL * 60 * 1000;
+            if (dataType == "application/json"){
+                data = JSON.stringify(data);
+            } else if (dataType == "application/xml"){
+                var oSerializer = new XMLSerializer();
+                data = oSerializer.serializeToString(data);
+            }
+            var record = {value: data, timestamp: new Date().getTime() + TTL}
+            window[storageType].setItem(key, JSON.stringify(record));
+            return data;
+        },
+        load : function(key, storageType){
+            if (!(storageAvailable(storageType))){return false;}
+            var record = JSON.parse(window[storageType].getItem(key));
+            if (!record){
+                return false;
+            }
+            return (new Date().getTime() < record.timestamp && JSON.parse(record.value));
+        }
+    }
+    // 
+    // Function for testing the availability of browser storage.
+    // 
+    function storageAvailable(type) {
+        try {
+            var storage = window[type],
+                x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        }
+        catch(e) {
+            return false;
+        }
+    }
+
+    return {
+        cachedAjaxPromise: cachedAjaxPromise,
+        percStorage: percStorage
+    }
+
+} (jQuery));
